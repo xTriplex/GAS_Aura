0a4e53b593e4ee556e0fd89c4c12907b6bd89958
Kaan Ayyıldız <kaan_xtriplex@hotmail.com>
2024-01-29 17:08:11 +0300

    Setting Up Forward and Right Rotation

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -6,7 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+struct FInputActionValue;
 class UInputMappingContext;
+class UInputAction;
 
 /**
  * 
@@ -19,7 +21,14 @@ public:
 	AAuraPlayerController();
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 
 private:
+	UPROPERTY(EditAnywhere, Category = "Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
 };
